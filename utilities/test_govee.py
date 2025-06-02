0ded9f07532f932cfa75f4159f76820416c3c082
Julius <s_boeger24@stud.hwr-berlin.de>
2025-06-02 16:33:19 +0200

    working colour change commit

--- utilities/test_govee.py
+++ utilities/test_govee.py
@@ -1,25 +1,39 @@
 import asyncio
-from govee_api_laggat import Govee
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from govee_api_laggat import Govee, GoveeAbstractLearningStorage, GoveeLearnedInfo
+from typing import Dict
 
-load_dotenv()
-API_KEY = os.getenv("GOVEE_API_KEY")
 
-async def main(): #async function so that program is not blocked while waiting for the API response
-    govee = Govee(api_key=API_KEY)
-    devices = await govee.get_devices()
+class YourLearningStorage(GoveeAbstractLearningStorage):
+    async def read(self) -> Dict[str, GoveeLearnedInfo]:
+        return {}
 
-    if not devices:
-        print("No device found.")
+    async def write(self, learned_info: Dict[str, GoveeLearnedInfo]):
+        pass
+
+
+async def main():
+    load_dotenv()
+    api_key = os.getenv("GOVEE_API_KEY")
+    if not api_key:
+        print("API key missing. Set GOVEE_API_KEY in .env.")
         return
 
-    device = devices[0]
-    print(f"Control device: {device.device_name}")
-    await govee.turn_on(device)
-    await govee.set_color(device, 255, 0, 0)  #red
+    async with Govee(api_key, learning_storage=YourLearningStorage()) as govee:
+        devices, err = await govee.get_devices()
+        if err or not devices:
+            print("No devices found or API error:", err)
+            return
+
+        device = govee.device(devices[0].device)
+        print(device)
+        await govee.turn_on(device)
+        await govee.set_color(device, (255, 0, 0))  # red
+
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
 # run python test_govee.py in terminal (bulb needs to be on and connected to the same network -> mobile hotspot)
