3045de63cd2cdc8855f359e95c797a4d3cdc7b6f
HoangHo <s_ho24@stud.hwr-berlin.de>
2025-06-10 13:51:05 +0200

    revise HeartRateProcessor class

--- utilities/heart_rate.py
+++ utilities/heart_rate.py
@@ -1,24 +1,49 @@
+# utilities/heart_rate.py
+
 import pandas as pd
 
-df = pd.read_csv('utilities\heart_rate_data\heartrate_seconds_merged.csv', parse_dates=['Time'])
-df_user1 = df[df['Id'] == 2022484408]
-# df_user1.loc[:,'Time'] = pd.to_datetime(df_user1['Time'])
-df_user1_30min = df_user1
-df_user1_30min = df_user1.resample('30Min', on='Time').mean().reset_index()
-df_user1_30min = df_user1_30min.rename(columns={'Value': 'avg_30_min'})
-df_user1_30min = df_user1_30min[['Id', 'Time', 'avg_30_min']]
-df_user1_30min = df_user1_30min.dropna()
+class HeartRateProcessor:
+    def __init__(self, csv_path, user_id):
+        self.csv_path = csv_path
+        self.user_id = user_id
+        self.df_raw = None
+        self.df_30min = None
 
+    def load_data(self):
+        self.df_raw = pd.read_csv(self.csv_path, parse_dates=['Time'])
+        self.df_raw = self.df_raw[self.df_raw['Id'] == self.user_id]
 
-def calculate_stress_score(avg_30_min):
-  if avg_30_min < 65:
-    return 0
-  elif avg_30_min > 120:
-    return 1
-  else:
-    # Linear interpolation between 65 and 120
-    return (avg_30_min - 65) / (120 - 65)
+    def calculate_rolling_avg(self):
+        df = self.df_raw.copy()
+        df = df.set_index('Time').resample('30min').mean().reset_index()
+        df = df.rename(columns={'Value': 'avg_30_min'})
+        df = df[['Time', 'avg_30_min']].dropna()
+        self.df_30min = df
 
-df_user1_30min['stress_score'] = df_user1_30min['avg_30_min'].apply(calculate_stress_score)
+    @staticmethod
+    def calculate_stress_score(avg_30_min):
+        if avg_30_min < 65:
+            return 0
+        elif avg_30_min > 120:
+            return 1
+        else:
+            return (avg_30_min - 65) / (120 - 65)
 
-print(df_user1_30min.head())
+    def add_stress_score(self):
+        self.df_30min['stress_score'] = self.df_30min['avg_30_min'].apply(self.calculate_stress_score)
+
+    def get_processed_df(self):
+        return self.df_30min
+
+#Main execution
+processor = HeartRateProcessor(
+    csv_path='utilities/heart_rate_data/heartrate_seconds_merged.csv',
+    user_id=2022484408
+)
+
+processor.load_data()
+processor.calculate_rolling_avg()
+processor.add_stress_score()
+
+df_result = processor.get_processed_df()
+print(df_result)
